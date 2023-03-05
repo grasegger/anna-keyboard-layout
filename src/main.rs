911214c456bf5f4a2cb85efac479e745c0796f1a
Karl <karlgrasegger@posteo.de>
2023-03-05 08:49:54 +0100

    feat: Moved layout checking to own function

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,19 @@ mod ngram;
 mod score;
 
 pub fn main() {
-    let raw_layouts = fs::read_to_string("layouts.list").unwrap();
-    let layouts: Vec<&str> = raw_layouts.split("\n").collect_vec();
-
-    let scores: Vec<Score> = layouts
-        .par_iter()
-        .map(|layout| Score::calculate(layout))
-        .collect();
+    let scores = read_and_calc_layouts();
 
     for score in scores {
         println!("{}\n", score);
     }
+}
+
+pub fn read_and_calc_layouts() -> Vec<Score> {
+    let raw_layouts = fs::read_to_string("layouts.list").unwrap();
+    let layouts: Vec<&str> = raw_layouts.split("\n").collect_vec();
+
+    layouts
+        .par_iter()
+        .map(|layout| Score::calculate(layout))
+        .collect()
 }
